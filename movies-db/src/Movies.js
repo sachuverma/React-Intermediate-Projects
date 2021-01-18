75377aa51058b569fdc1a874016cd6a4ccf00580
Sachin Verma <sachinverma53121@gmail.com>
2021-01-18 11:50:07 +0530

    movies list

--- movies-db/src/Movies.js
+++ movies-db/src/Movies.js
@@ -19,10 +19,13 @@ const Movies = () => {
         const { imdbID: id, Poster: poster, Title: title, Year: year } = movie;
         return (
           <Link to={`/movies/${id}`} key={id} className="movie">
-            <div className="movie-info">
-              <h4 className="title">{title}</h4>
-              <p>{year}</p>
-            </div>
+            <article>
+              <img src={poster === "N/A" ? url : poster} alt={title} />
+              <div className="movie-info">
+                <h4 className="title">{title}</h4>
+                <p>{year}</p>
+              </div>
+            </article>
           </Link>
         );
       })}
