3d49b8c02c5f829e3832fc3d29678781c043d18c
Sachin Verma <sachinverma53121@gmail.com>
2021-01-18 11:08:20 +0530

    movies api

---
+++ movies-db/src/SearchForm.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+import { useGlobalContext } from "./context";
+
+const SearchForm = () => {
+  const {} = useGlobalContext();
+  return <h2>search component</h2>;
+};
+
+export default SearchForm;
